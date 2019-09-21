27b5bb67afec95b61df5e69d1e647fb131176476
PhantomGrin <savindi.wijenayaka@gmail.com>
2019-09-21 13:20:33 +0530

    added reading file

--- src/main/java/com/project/tda/controllers/EndPointController.java
+++ src/main/java/com/project/tda/controllers/EndPointController.java
@@ -1,7 +1,11 @@
 package com.project.tda.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class EndPointController {
@@ -9,5 +13,16 @@ public class EndPointController {
     @GetMapping("/")
     public String welcome(){
         return "Welcome";
+    }
+
+    @GetMapping("/analyze")
+    public String Analyze(@RequestParam MultipartFile file){
+        String content = "Test";
+        try {
+            content = new String(file.getBytes(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
     }
 }
